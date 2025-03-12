90147363f94520dedd5c9344755ca14e71c2e6d4
jimyung <sjm77773@naver.com>
2025-03-12 14:35:26 +0900

    add: add OOP version code

--- calculating postfix expression with stack.cpp
+++ calculating postfix expression with stack.cpp
@@ -134,3 +134,96 @@ int main()
 
 	return 0;
 }
+
+
+//OOP version
+
+#include<iostream>
+#include<string>
+#include<cctype>
+#define MAX 10000
+using namespace std;
+
+// Postfix Expression Calculate
+class PEC {
+private:
+	int stack[MAX];
+	int top;
+	int nn, cn;
+	string expression;
+public:
+	PEC() : top(-1), nn(0), cn(0) {
+		cout << "The postfix expression calculating program\n" << endl;
+	}
+
+	void enterExpression() {
+		cout << "Enter the postfix exrpession >> ";
+		getline(cin, expression);
+	}
+
+	void push(int x) {
+		if (top < MAX - 1) {
+			top++;
+			stack[top] = x;
+			nn++;
+		}
+	}
+
+	void calculation(char x) {
+		if (top >= 1) {
+			if (x == '+') {
+				stack[top - 1] = stack[top - 1] + stack[top];
+			}
+			if (x == '-') {
+				stack[top - 1] = stack[top - 1] - stack[top];
+			}
+			if (x == '*') {
+				stack[top - 1] = stack[top - 1] * stack[top];
+			}
+			if (x == '/') {
+				stack[top - 1] = stack[top - 1] / stack[top];
+			}
+			top--;
+		}
+		cn++;
+	}
+
+	void solve() {
+		int len = expression.length();
+
+		for (int i = 0; i < len; i++) {
+			if (expression[i] != ' ') {
+				if (isdigit(expression[i]) && isdigit(expression[i + 1]) && !isdigit(expression[i + 2])) {
+					int x = (expression[i] - '0') * 10 + (expression[i + 1] - '0');
+					push(x);
+					i++;
+				}
+				else if (isdigit(expression[i]) && !isdigit(expression[i + 1])) {
+					int x = (expression[i] - '0');
+					push(x);
+				}
+				else {
+					calculation(expression[i]);
+				}
+			}
+		}
+		nn - 1 == cn ? cout << stack[0] << endl : cout << "Invalid expression" << endl;
+	}
+	~PEC() {
+		for (int i = 0; i <= top; i++)stack[i] = 0;
+		top = -1, cn = 0, nn = 0;
+	}
+};
+
+
+int main()
+{
+
+	PEC Pssion;
+
+	Pssion.enterExpression();
+
+	Pssion.solve();
+
+	return 0;
+}
